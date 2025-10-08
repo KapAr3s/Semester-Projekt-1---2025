5a78a896eaf813c5fb2b593f5a9321166e65cccd
lucasnguyen4823-cpu <lucasnguyen4823@gmail.com>
2025-10-08 10:43:32 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,10 +3,9 @@ print("world")
 
 print("from")
 
-<<<<<<< HEAD
 print("test")
-=======
 print("Dav")
 
 print("jeppes test")
->>>>>>> 92eb8030e2197c81229852dc30ddd170ee18c92d
+
+print("endu en test")
