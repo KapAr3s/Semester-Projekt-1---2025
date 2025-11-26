0fa425ee4fc1bded0c6310debbae5d1f7070532d
JeppeGlasdamSDU <jegla25@student.sdu.dk>
2025-11-26 13:30:43 +0100

    Add files via upload

--- differential_drive.py
+++ differential_drive.py
@@ -1,151 +1,76 @@
-from stepper_motor import StepperMotor
-import uasyncio as asyncio
+from stepper_motor2 import StepperMotor
+from machine import Timer
 import time
 import math
 
+tim = Timer()
+
+STEPS_PER_REVOLUTION = 200
+WHEEL_DIAMETER_MM = 85
+CIRCUMFERENCE_MM = WHEEL_DIAMETER_MM * math.pi
+STEP_DISTANCE_MM = CIRCUMFERENCE_MM / STEPS_PER_REVOLUTION
+WHEEL_BASE_MM = 240
+WHEEL_BASE_CIRCUMFERENCE_MM = WHEEL_BASE_MM * math.pi
+WHEEL_BASE_CIRCUMFERENCE_STEPS = WHEEL_BASE_CIRCUMFERENCE_MM / STEP_DISTANCE_MM
+
 class DifferentialDrive:
-    def __init__(self, left, right):
+    STATUS_IDLE = 0 
+    STATUS_JOB = 1
+    def __init__(self, left = StepperMotor([0,1,2,3]), right = StepperMotor([4,5,6,7])):
         """
         Initialize the navigation system with two stepper motors.
 
         :param left: Instance of StepperMotor class for the left motor.
         :param right: Instance of StepperMotor class for the right motor.
         """
-
         self.left = left
         self.right = right
-        self.stop()
+        self.max_odometer = 0
+        self.set_speed(0,0)
 
-    def stop(self):
-        self.left.stop()
-        self.right.stop()
-        
-    def move_stepper(self, cycles, direction, delay_us=1000):
-        """
-        Move the stepper motor a specified number of steps.
+        self.status = DifferentialDrive.STATUS_IDLE
 
-        :param cycles: Number of cycles(4 steps) to move. Positive for forward, negative for backward.
-        :param direction: Direction to move. "forward" for forward, "backward" for backward.
-        :param delay_us: Delay between steps in microseconds.
-        """
+    def set_speed(self, left, right):
+        tim.deinit()
+        self.left.set_speed(left)
+        self.right.set_speed(right)
+        tim.init(period=1, mode=Timer.PERIODIC, callback=self.drive)
+
+    def drive(self, timer):
+        self.left.step()
+        self.right.step()
+        if self.max_odometer > 0 and max(self.left.odometer, self.right.odometer) >= self.max_odometer:
+            self.set_speed(0,0)
+            self.max_odometer = 0
+            self.status = DifferentialDrive.STATUS_IDLE
+
+    def destroy(self):
+        tim.deinit()
+        self.set_speed(0,0)
+        self.drive(None)
+
+    def move_steps(self, steps, left_speed, right_speed = None):
+        self.status = DifferentialDrive.STATUS_JOB
+        self.left.odometer = 0
+        self.right.odometer = 0
+        self.max_odometer = steps
+        if right_speed == None:
+            right_speed = left_speed 
+        self.set_speed(left_speed, right_speed)
     
-        if direction == "forward":
-            sequence = self.sequence
-        elif direction == "backward":
-            sequence = list(reversed(self.sequence))
-        else:
-            raise ValueError("Direction must be 'forward' or 'backward'")
-        
-        for cycle in range(cycles):
-            for step in sequence:
-                self.left.set_step(step)
-                self.right.set_step(step)
-                time.sleep_us(delay_us)
-
-    def run(self, motor, steps):
-        motor.move_stepper(steps)
-
-    def drive(self, left_steps, right_steps, delay_us):
-        self.left.move_stepper(left_steps, delay_us)
-
-    def forward(self, steps, delay_us=1000):
-        """
-        Move both motors forward a specified number of steps.
-        :param steps: Number of steps to move forward.
-        :param delay_us: Delay between steps in microseconds.
-        """
-        self.drive(steps, steps, delay_us)
-        self.stop()
-
-    def backward(self, steps, delay_us=1000):
-        """
-        Move both motors backward a specified number of steps.
-        :param steps: Number of steps to move backward.
-        :param delay_us: Delay between steps in microseconds.
-        """
-        self.drive(-steps, -steps, delay_us)
-        self.stop()
-
-    def mm_to_steps(self, distance_mm):
-        """
-        Convert a distance in centimeters to the corresponding number of motor steps.
-        :param distance_cm: Distance to move in centimeters.
-        :return: Number of steps corresponding to the given distance.
-        """
-        
-        wheel_diameter_mm = 85
-        steps_per_revolution = 200
-        steps_per_sequence = 4
-        
-        circumference = wheel_diameter_mm*math.pi
-        distance_per_step = (steps_per_sequence/steps_per_revolution)*circumference
-        steps = distance_mm / distance_per_step
-        steps = round(steps)
-        return steps
-    
-    def degrees_to_steps(self, degrees, turning_radius_mm):
-        '''
-        Convert a degree to number of steps to turn.
-        :param degrees (int): degrees to turn
-        :param turning_radius_mm (int): turning radius in mm
-        :return (int): number of steps
-        '''
-        #240 mm from wheel to wheel
-        #120 mm from wheel to the centre between the two wheels
-        wheel_diameter_mm = 85
-        steps_per_revolution = 200
-        steps_per_sequence = 4
-    
-        wheel_circumference_mm = math.pi * wheel_diameter_mm
-        turning_circumference_mm = 2 * math.pi * turning_radius_mm
-
-        distance_per_wheel_mm = (degrees / 360) * turning_circumference_mm
-        revolutions = distance_per_wheel_mm / wheel_circumference_mm
-        total_steps = self.mm_to_steps(distance_per_wheel_mm)
-        return total_steps
-
-    def move_distance(self, distance_mm, direction, delay_us=1000):
-        """
-        Move the robot forward or backward a specific distance in centimeters.
-
-        :param distance_cm: Distance to move in centimeters.
-        :param direction: Direction to move, either 'forward' or 'backward'.
-        :param delay_us: Delay between steps in microseconds.
-        """
-
-        steps = self.mm_to_steps(distance_mm)
-
-        self.move_stepper(steps, direction, delay_us)
-
-        self.stop()
-        
-    def turn_in_place(self, degrees, direction, delay_us=1000):
+    def move_distance_mm(self, distance_mm, left_speed, right_speed = None):
+        steps = round(distance_mm / STEP_DISTANCE_MM)
+        self.move_steps(steps, left_speed, right_speed)
+                    
+    def turn_in_place(self, degrees, speed = 1):
         """
         Turn the robot a specific number of degrees.
 
         :param degrees: The angle in degrees to turn.
-        :param direction: Direction to turn, either 'left' or 'right'.
-        :param delay_us: Delay between steps in microseconds.
+        :param speed: How fast can it go round?
         """
-        
-        steps = self.degrees_to_steps(degrees, 120)
+        if degrees < 0:
+            speed = -speed
 
-        if direction == "left":
-            # Left wheel moves backward, right wheel moves forward
-            for step in range(steps):
-                for n in range(len(self.sequence)):
-                    self.left.set_step(self.sequence[-(n+1)])
-                    self.right.set_step(self.sequence[n])
-                    time.sleep_us(delay_us)
-           
-        elif direction == "right":
-            # Right wheel moves backward, left wheel moves forward
-            for step in range(steps):
-                for n in range(len(self.sequence)):
-                    self.left.set_step(self.sequence[n])
-                    self.right.set_step(self.sequence[-(n+1)])
-                    time.sleep_us(delay_us)
-        else:
-            raise ValueError("Direction must be 'left' or 'right'")
-
-        self.stop()
+        steps = abs(round(WHEEL_BASE_CIRCUMFERENCE_STEPS * degrees / 360))
+        self.move_steps(steps, left_speed=speed, right_speed= -speed)
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from stepper_motor import StepperMotor
-from differential_drive import DifferentialDrive
 import time
+from stepper_motor2 import StepperMotor
+from differential_drive import DifferentialDrive
 
-# LED pinout 16, 17, 18, 19, 20, 21, 22, 26
-# motor pinout left: 0, 1, 2, 3 og right: 4, 5, 6, 7
-left_motor = StepperMotor([16, 17, 18, 19], pwm_pct=0.7, frequency=16_000)
-right_motor = StepperMotor([20, 21, 22, 26], pwm_pct=0.7, frequency=16_000)
+left = StepperMotor([0,1,2,3])
+right = StepperMotor([4,5,6,7])
+diff_drive = DifferentialDrive(left, right)
 
-robot = DifferentialDrive(left_motor, right_motor)
-robot.forward(1)
+# for i in range(11):
+#   diff_drive.set_speed(i, -i)
+#   print(left.speed)
+#   time.sleep(1)
 
-#robot.forward(1)
-#robot.move_distance(267, "forward")
-#print(robot.degrees_to_steps(360, 120))
+for _ in range(4):
+  diff_drive.move_distance_mm(500, 9)
+  while diff_drive.status == diff_drive.STATUS_JOB:
+    time.sleep(0.1)
+  diff_drive.turn_in_place(-90, 6)
+  while diff_drive.status == diff_drive.STATUS_JOB:
+    time.sleep(0.1)
 
-#robot.move_distance(1000, "backward", 500)
-#robot.move_distance(1000, "forward", delay_us=1)
-#robot.turn_in_place(360, "left", delay_us=1)
+
+diff_drive.destroy()
