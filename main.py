1ada86465d4110e48c6904ca9beec5afdec00c8c
Business-Monkey <malthe.stokkebro.tonnesen@gmail.com>
2025-12-11 11:03:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from stepper_motor import StepperMotor
 from differential_drive import DifferentialDrive
+from LDR import I2C_ADS_LDR
 import time
 
 # LED pinout 16, 17, 18, 19, 20, 21, 22, 26
 # motor pinout left: 0, 1, 2, 3 og right: 4, 5, 6, 7
 left_motor = StepperMotor([16, 17, 18, 19], pwm_pct=0.7, frequency=16_000)
 right_motor = StepperMotor([20, 21, 22, 26], pwm_pct=0.7, frequency=16_000)
+adc = I2C_ADS_LDR(0x48,[0,3,4,7])
 
 robot = DifferentialDrive(left_motor, right_motor)
 robot.forward(1)
