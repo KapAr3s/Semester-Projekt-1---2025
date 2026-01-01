3dc3e0d4b795796638f0cc90825690a4b3dd4d60
JeppeGlasdamSDU <jegla25@student.sdu.dk>
2026-01-01 17:31:35 +0100

    Add files via upload

---
+++ differential_drive.py
@@ -0,0 +1,102 @@
+from stepper_motor import StepperMotor
+from machine import Timer
+import time
+import math
+
+tim = Timer()
+
+STEPS_PER_REVOLUTION = 200
+WHEEL_DIAMETER_MM = 85
+CIRCUMFERENCE_MM = WHEEL_DIAMETER_MM * math.pi
+STEP_DISTANCE_MM = CIRCUMFERENCE_MM / STEPS_PER_REVOLUTION
+WHEEL_BASE_MM = 240
+WHEEL_BASE_CIRCUMFERENCE_MM = WHEEL_BASE_MM * math.pi
+WHEEL_BASE_CIRCUMFERENCE_STEPS = WHEEL_BASE_CIRCUMFERENCE_MM / STEP_DISTANCE_MM
+
+class DifferentialDrive:
+    STATUS_IDLE = 0 
+    STATUS_JOB = 1
+    def __init__(self, left = StepperMotor([0,1,2,3]), right = StepperMotor([4,5,6,7])):
+        """
+        Initialize the navigation system with two stepper motors.
+
+        :param left: Instance of StepperMotor class for the left motor.
+        :param right: Instance of StepperMotor class for the right motor.
+        """
+        self.left = left
+        self.right = right
+        self.max_odometer = 0
+        self.set_speed(0,0)
+
+        self.status = DifferentialDrive.STATUS_IDLE
+
+    def set_speed(self, left_speed, right_speed):
+        tim.deinit()
+        # the direction of travel is in this case reveresed from the way stepper motor class is designed.
+        self.left.set_speed(-left_speed) 
+        self.right.set_speed(-right_speed)
+        tim.init(period=1, mode=Timer.PERIODIC, callback=self.drive)
+
+    def drive(self, timer):
+        self.left.step()
+        self.right.step()
+        if self.max_odometer > 0 and max(self.left.odometer, self.right.odometer) >= self.max_odometer:
+            self.set_speed(0,0)
+            self.max_odometer = 0
+            self.status = DifferentialDrive.STATUS_IDLE
+
+    def destroy(self):
+        self.set_speed(0,0)
+        tim.deinit()
+        self.drive(None)
+
+    def move_steps(self, steps, left_speed, right_speed = None):
+        self.status = DifferentialDrive.STATUS_JOB
+        self.left.odometer = 0
+        self.right.odometer = 0
+        self.max_odometer = steps
+        if right_speed == None:
+            right_speed = left_speed 
+        self.set_speed(left_speed, right_speed)
+    
+    def move_distance_mm(self, distance_mm, left_speed, right_speed = None):
+        steps = round(distance_mm / STEP_DISTANCE_MM)
+        self.move_steps(steps, left_speed, right_speed)
+                    
+    def turn_in_place(self, degrees, speed = 1):
+        """
+        Turn the robot a specific number of degrees.
+
+        :param degrees: The angle in degrees to turn.
+        :param speed: How fast can it go round?
+        """
+        if degrees < 0:
+            speed = -speed
+
+        steps = abs(round(WHEEL_BASE_CIRCUMFERENCE_STEPS * degrees / 360))
+        self.move_steps(steps, left_speed=speed, right_speed= -speed)
+
+    def accelerate(self, terminal_speed):
+        # no neeed to "slowly" accelerate to speed 7 
+        if abs(terminal_speed) <= 7:
+            self.set_speed(terminal_speed, terminal_speed)
+        else:
+            direction = 1
+            if terminal_speed < 0:
+                direction = -1
+            for i in range(7, abs(terminal_speed)+1):
+                self.set_speed(i*direction, i*direction)
+                time.sleep_us(100000) #0.1 seconds 
+    
+    #only forward
+    def left_accelerate(self):
+        for i in range(7, 10):
+            self.set_speed(i, i)
+            time.sleep_us(100000) #0.1 seconds 
+        self.set_speed(10, 9)
+    
+    def right_accelerate(self):
+        for i in range(7, 10):
+            self.set_speed(i, i)
+            time.sleep_us(100000) #0.1 seconds 
+        self.set_speed(9, 10)
---
+++ stepper_motor.py
@@ -0,0 +1,82 @@
+from machine import Pin, PWM
+
+PWM_MAX = 65535
+
+class StepperMotor:
+
+  def __init__(self, pins, pwm_pct = 0.15, frequency=18_000):
+    self.direction = 0
+    self.sequence = [[0,0,0,0]]
+    self.odometer = 0
+    self.speed = 0
+    self.step_index = 0
+    self.pwm_val = int(PWM_MAX * pwm_pct)
+    self.pins = [PWM(Pin(pin)) for pin in pins]
+    for pin in self.pins:
+      pin.freq(frequency)
+
+  def print(self):
+    print("odomter, speed(step_size), step, step index, direction")
+    print([self.odometer, self.speed, self.sequence[self.step_index], self.step_index, self.direction])
+
+  def step(self):
+    self.step_index = (self.step_index + self.direction) % len(self.sequence)
+    step = self.sequence[self.step_index]
+    for pin in range(len(self.pins)):
+      self.pins[pin].duty_u16(step[pin])
+
+    if self.speed != 0 and self.step_index % (self.speed*2) == 0:
+       self.odometer += 1
+
+  def set_speed(self, speed):
+    if abs(speed) > 10:
+      self.speed = 1
+    else:
+      self.speed = 11-abs(speed)
+    if speed < 0:
+      direction = -1
+    elif speed == 0:
+      direction = 0
+      self.speed = 0
+    else:
+      direction = 1
+
+    orginal_seq_len = len(self.sequence)
+    self.sequence = self.create_sequence(self.speed)
+    self.step_index = int((self.step_index*len(self.sequence))/orginal_seq_len)
+    self.direction = direction
+
+  def create_sequence(self, size):
+    '''
+    Create new micro steps.
+    :param size (int): the size of the microstep e.g. 3 then the micro steps is going pwm_val*1//3, pwm_val*2//3 and pwm_val,
+    before is going down.
+    '''
+    if size == 0:
+      return [[0,0,0,0]]
+    one_step = []
+    four_phases = []
+    
+    # One coil completely on, the next coil varming up 
+    for i in range(size+1):
+      one_step.append([self.pwm_val, self.pwm_val*i//size])
+    # The one that was on is now slowly cooling of 
+    for u in reversed(range(1, size)):
+      one_step.append([self.pwm_val*u//size, self.pwm_val])
+    
+    # We now shift the one_step array in our sequence four times.  
+    for u in range(4):
+      # The lenght of sequence depends on the one_step size
+      for i in range(len(one_step)):
+        elements = [0,0,0,0]
+        a = one_step[i][0]
+        b = one_step[i][1]
+        elements[u] = a
+        # When index is longer than list is it because the value needs to be in the start.  
+        if u+1 >= len(elements):
+          elements[0] = b
+        else:
+          elements[u+1] = b
+        four_phases.append(elements)        
+    
+    return four_phases
